574208a36d0f6e143ec884914ca14b6dc5cb419b
whs <hswongac@gmail.com>
2020-12-29 21:21:53 +0800

    my_makemkv

--- start_makemkv.sh
+++ start_makemkv.sh
@@ -1,15 +1,15 @@
 bazel build @makemkv-bin//:binary
 bazel build @makemkv-oss//:binary
 
-rm -rf makemkv
+rm -rf my_makemkv
 
-mkdir makemkv
+mkdir my_makemkv
 
-find -L bazel-bin/*/makemkv-*/binary -type f -and \( -name libdriveio.so.0 -or -name libmakemkv.so.1 -or -name libmmbd.so.0 -or -name makemkv -or -name makemkvcon \) -exec cp {} makemkv \;
+find -L bazel-bin/*/makemkv-*/binary -type f -and \( -name libdriveio.so.0 -or -name libmakemkv.so.1 -or -name libmmbd.so.0 -or -name makemkv -or -name makemkvcon \) -exec cp {} my_makemkv \;
 
-cd makemkv
+cd my_makemkv
 
 chmod a+w makemkvcon
-patchelf --set-rpath . makemkvcon
+patchelf --set-rpath `pwd` makemkvcon
 
 ./makemkv

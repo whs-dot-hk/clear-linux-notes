e1095d407f1de67a25d4504f8a0c845ff30f5ebb
whs <hswongac@gmail.com>
2021-05-28 18:29:40 +0800

    Add youtube like

---
+++ userscript_youtube_like.js
@@ -0,0 +1,33 @@
+// ==UserScript==
+// @name Youtube like
+// @match https://www.youtube.com/*
+// ==/UserScript==
+document
+  .querySelector("ytd-app")
+  .addEventListener("yt-page-data-updated", function () {
+    const likeButton = document.querySelector(
+      "ytd-menu-renderer.ytd-video-primary-info-renderer > div:nth-child(2) > ytd-toggle-button-renderer:nth-child(1) > a:nth-child(1)"
+    );
+
+    const videoTitle = document.querySelector(
+      "yt-formatted-string.ytd-video-primary-info-renderer:nth-child(1)"
+    ).textContent;
+
+    if (window.location.pathname !== "/watch") {
+      return;
+    }
+
+    (function liked() {
+      return (
+        likeButton.classList.contains("style-default-active") &&
+        (function log() {
+          console.log("Liked!");
+          return true;
+        })()
+      );
+    })() ||
+      (function like() {
+        likeButton.click();
+        console.log(`Liked! ${videoTitle}`);
+      })();
+  });
